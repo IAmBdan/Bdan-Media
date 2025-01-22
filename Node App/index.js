456b579923b889ad9eccefa7e71e406bc07d2ccd
Brian Daniels <daniels.bri@northeastern.edu>
2025-01-22 15:12:22 -0500

    update server ports

--- Node App/index.js
+++ Node App/index.js
@@ -27,6 +27,6 @@ app.use((req, res, next) => {
 
 // Starts the server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });

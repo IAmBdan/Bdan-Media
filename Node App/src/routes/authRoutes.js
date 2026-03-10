a491689181408619438fab8614f477e26aaef29f
Brian Daniels <daniels.bri@northeastern.edu>
2026-03-10 03:28:42 -0400

    ui changes

--- Node App/src/routes/authRoutes.js
+++ Node App/src/routes/authRoutes.js
@@ -1,12 +1,8 @@
+
 const express = require('express');
-const { loginUser, registerUser} = require('../controllers/userController');
+const { loginUser } = require('../controllers/userController');
 const router = express.Router();
-const authenticate = require('../middleware/auth');
 
-
-router.post('/login', loginUser); // Login
-router.post('/register', registerUser); // Register
-
-
+router.post('/login', loginUser);
 
 module.exports = router;
